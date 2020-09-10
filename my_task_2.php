fa96171287c94937c72b0368e9fba718d5001297
Oleg <o.kazanin@outlook.com>
2020-09-10 18:01:31 +0300

    Update my_task_2.php
    Изменен php синтаксис

--- my_task_2.php
+++ my_task_2.php
@@ -31,24 +31,23 @@
                         </div>
                     </div>
                     <?php
-                        $arr = array(
+                        $array = array(
                             "Privacy" => "Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
                                 We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
                                 We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
                                 We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.",
                             "Cookies and other similar technologies" => "We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings"
                         );
-                        //var_dump($arr);
                     ?>
                     <div class="panel-container show">
                         <div class="panel-content">
                             <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
-                            <?php
-                            foreach ($arr as $key => $value) {
-                                echo ('<h2>'); echo "$key"; echo('</h2>');
-                                echo ('<p class="mb-g">'); echo "$value"; echo('<p>');
-                            }
-                            ?>
+                            <?php foreach ($array as $key => $arr): ?>
+                                <h2><?php echo $arr['key']; ?></h2>
+                                <p class="mb-g"><?php echo $arr['arr']; ?></p>
+                                <h3><?php echo $arr['key']; ?></h3>
+                                <p><?php echo $arr['arr']; ?></p>                           
+                            <?php endforeach?>
                         </div>
                         </div>
                     </div>
